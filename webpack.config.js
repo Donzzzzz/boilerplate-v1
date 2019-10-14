6c516afacb78fbb9eaf93136ecc4ae5e4629e438
edom <edom.zhang@gmail.com>
2019-10-14 11:39:27 +1100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// entry -> output
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        // ? mean we can look for css or scss, the s is a choice
+        test: /\.s?css$/,
+        // use can set an array of loader
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true
+  }
+};
